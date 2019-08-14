4b99cbbf63ae8176c4407d62d49cb5aa5c2c9aa1
Sathish Pittala <48255299+sathishbaburaj@users.noreply.github.com>
2019-08-13 23:42:26 -0400

    Made changes to spotifySearch() to print the song information for more artists

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 // Grab the axios package...
 var axios = require("axios");
-var inquirer =require("inquirer");
+var inquirer = require("inquirer");
 // Core node package for reading and writing files
 var fs = require("fs");
 require("dotenv").config();
@@ -21,7 +21,7 @@ function runSearch() {
         "concert-this",
         "do-what-it-says",
         "Exit"
-              ]
+      ]
     })
     .then(function(answer) {
       switch (answer.action) {
@@ -41,7 +41,7 @@ function runSearch() {
           doWhatItSays();
           break;
         }
-        case "Exit":{
+        case "Exit": {
           console.log("Exiting the application");
           break;
         }
@@ -50,78 +50,66 @@ function runSearch() {
 }
 runSearch();
 function movieThis() {
-  
   inquirer
     .prompt({
       name: "movie",
       type: "input",
       message: "What movie would you like to search for?",
-      default : "Mr."+" Nobody",
+      default: "Mr." + " Nobody"
     })
     .then(function(answer) {
       var queryUrl =
-    "http://www.omdbapi.com/?t=" +
-    answer.movie +
-    "&y=&plot=short&apikey=" +
-    keys.movies.id;
- // Then run a request with axios to the OMDB API with the movie specified
-  axios.get(queryUrl).then(
-    function(response, error) {
-      var movieObj = {
-        "Title of the Movie: ": response.data.Title,
-        "Realeased Year: ": response.data.Year,
-        "IMDB Rating: ": response.data.imdbRating,
-        "Country where the movie was produced: ": response.data.Country,
-        "Language of the Movie: ": response.data.Language,
-        "Plot of the Movie: ": response.data.Plot,
-        "Actors in the Movie: ": response.data.Actors
-      };
+        "http://www.omdbapi.com/?t=" +
+        answer.movie +
+        "&y=&plot=short&apikey=" +
+        keys.movies.id;
+      // Then run a request with axios to the OMDB API with the movie specified
+      axios.get(queryUrl).then(
+        function(response, error) {
+          var movieObj = {
+            "Title of the Movie: ": response.data.Title,
+            "Realeased Year: ": response.data.Year,
+            "IMDB Rating: ": response.data.imdbRating,
+            "Country where the movie was produced: ": response.data.Country,
+            "Language of the Movie: ": response.data.Language,
+            "Plot of the Movie: ": response.data.Plot,
+            "Actors in the Movie: ": response.data.Actors
+          };
 
-      Array.from(Object.keys(movieObj)).forEach(function(key) {
-        console.log(key + movieObj[key]);
-        logThis(key + movieObj[key]);
-      });
-    },
-    function(error) {
-      console.log(error);
-    }
-  );
-       
+          Array.from(Object.keys(movieObj)).forEach(function(key) {
+            console.log(key + movieObj[key]);
+            logThis(key + movieObj[key]);
+          });
+        },
+        function(error) {
+          console.log(error);
+        }
+      );
     });
-    
-
- 
-  
 }
-
 
 function spotifyThis() {
-  if(!userQuery){
-  inquirer
-    .prompt({
-      name: "song",
-      type: "input",
-      message: "What song would you like to search for?",
-      default : "The"+" Sign"+" Ace"+" of"+" base",
-    })
+  if (!userQuery) {
+    inquirer
+      .prompt({
+        name: "song",
+        type: "input",
+        message: "What song would you like to search for?",
+        default: "The" + " Sign" + " Ace" + " of" + " base"
+      })
 
-    .then(function(answer) {
-      userQuery = answer.song;
-      spotifySearch();
-  
-    })
-    
-    .catch(function(error) {
-      console.log(error);
-    });
-  
-  
-  // spotifySearch();
-  
+      .then(function(answer) {
+        userQuery = answer.song;
+        spotifySearch();
+      })
+
+      .catch(function(error) {
+        console.log(error);
+      });
+
+    // spotifySearch();
+  }
 }
-}
-
-
 
 function concertThis() {
   inquirer
@@ -129,41 +117,43 @@ function concertThis() {
       name: "band",
       type: "input",
       message: "What band would you like to search for?",
-      default : "backstreet "+"boys",
+      default: "backstreet " + "boys"
     })
-  
-  // Then run a request with axios to the OMDB API with the movie specified
-  .then(function(answer) {
-    var queryUrl =
-    "https://rest.bandsintown.com/artists/" +
-    answer.band +
-    "/events?app_id=" +
-    keys.bands.id;
 
-  axios.get(queryUrl).then(
-    function(response, error) {
-      for (var i = 0; i < response.data.length; i++) {
-        var concertObj = {
-          "Venue name: ": response.data[i].venue.name,
-          "Venue Location: ": response.data[i].venue.city,
-          "Venue Country: ": response.data[i].venue.country,
-          "Date of the Event: ": moment(response.data[i].datetime).format("L"),
-          "------------------": "-------------------------------------------"
-        };
+    // Then run a request with axios to the OMDB API with the movie specified
+    .then(function(answer) {
+      var queryUrl =
+        "https://rest.bandsintown.com/artists/" +
+        answer.band +
+        "/events?app_id=" +
+        keys.bands.id;
 
-        Array.from(Object.keys(concertObj)).forEach(function(key) {
-          console.log(key + concertObj[key]);
-          logThis(key + concertObj[key]);
-        });
-      }
-    },
+      axios.get(queryUrl).then(
+        function(response, error) {
+          for (var i = 0; i < response.data.length; i++) {
+            var concertObj = {
+              "Venue name: ": response.data[i].venue.name,
+              "Venue Location: ": response.data[i].venue.city,
+              "Venue Country: ": response.data[i].venue.country,
+              "Date of the Event: ": moment(response.data[i].datetime).format(
+                "L"
+              ),
+              "------------------":
+                "-------------------------------------------"
+            };
 
-    function(error) {
-      console.log(error);
-    }
-  );
-});
+            Array.from(Object.keys(concertObj)).forEach(function(key) {
+              console.log(key + concertObj[key]);
+              logThis(key + concertObj[key]);
+            });
+          }
+        },
 
+        function(error) {
+          console.log(error);
+        }
+      );
+    });
 }
 
 function doWhatItSays() {
@@ -185,7 +175,6 @@ function doWhatItSays() {
   });
 }
 
-
 function logThis(logquery) {
   // This block of code will create a file called "logs.txt".
   // It will then print "Inception, Die Hard" in the file
@@ -199,24 +188,23 @@ function logThis(logquery) {
     // console.log("logs.txt was updated!");
   });
 }
-function spotifySearch(){
-spotify
+function spotifySearch() {
+  spotify
     .search({ type: "track", query: userQuery })
     .then(function(response, error) {
       var userSong = response.tracks.items;
-      
       for (var i = 0; i < userSong.length; i++) {
-        var songObj= userSong[i].album;
-        console.log("Artist(s) Name: "  + songObj.name);
+        console.log("Artist(s) :" + userSong[i].album.artists[0].name);
         console.log("Song's Name :" + userSong[i].name);
         console.log("Preview Link : " + userSong[i].preview_url);
         console.log("Album : " + userSong[i].album.name);
-        console.log("----------------------------------------------------------------------------------");
-
+        console.log(
+          "----------------------------------------------------------------------------------"
+        );
+        logThis("Artist(s) :" + userSong[i].album.artists[0].name);
         logThis("Song's Name :" + userSong[i].name);
         logThis("Preview Link : " + userSong[i].preview_url);
         logThis("Album : " + userSong[i].album.name);
-        
       }
-    });
-  }
+          });
+}
